e6b9a3a4a1903944993a70e5337dd54e999e81d8
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-06-14 20:21:00 -0300

    Implementação dos métodos save, update, delete e criação do método findByNome de ContatoService

--- contato/src/main/java/com/example/contato/contato/services/ContatoService.java
+++ contato/src/main/java/com/example/contato/contato/services/ContatoService.java
@@ -3,6 +3,7 @@ package com.example.contato.contato.services;
 import com.example.contato.contato.models.Contato;
 import com.example.contato.contato.repositories.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,14 +28,25 @@ public class ContatoService {
     }
 
     public Contato save(Contato contato) {
-
+        findByNome(contato);
+        return contatoRepository.save(contato);
     }
 
-    public Contato update(Contato map) {
+    public Contato update(Contato contato) {
+        findById(contato.getId());
+        findByNome(contato);
+        return contatoRepository.save(contato);
+    }
 
+    private void findByNome(Contato contato) {
     }
 
     public void delete(Integer id) {
-
+        findById(id);
+        Optional<Contato> con = contatoRepository.findById(id);
+        if(!con.get().getNome().isEmpty()) {
+            throw new DataIntegrityViolationException("Não é possível excluir um contato com esse nome citado.");
+        }
+            contatoRepository.deleteById(id);
     }
 }
